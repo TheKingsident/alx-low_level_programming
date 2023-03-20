4d8ae7e206d2543dbc687f38da1e4a37ad20240b
Kingsley Usa <kingsident@gmail.com>
2023-03-20 18:22:34 +0100

    Added new assignment directory and files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	char myString [60] = "Programming is like building a multilingual puzzle\n";
+	char myString[60] = "Programming is like building a multilingual puzzle\n";
 
 	puts(myString);
-	
+
 	return (0);
 }
