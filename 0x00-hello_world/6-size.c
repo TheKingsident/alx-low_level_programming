b1b39464304bcaf4f2848d3544ec547a79c3c4bc
Kingsley Usa <kingsident@gmail.com>
2023-03-21 00:37:43 +0100

    Added new assignment directory and files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,8 @@
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of an int: %zu byte(s)u\n", sizeof(int));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of an float: %zu byte(s)u\n", sizeof(float));
+	printf("Size of an float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
