cc284485b10451046e5b2f298a62b8c07d1e0087
Kingsley Usa <kingsident@gmail.com>
2023-03-26 20:28:21 +0100

    Added new assignment directory and files

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,10 +19,15 @@ int main(void)
 	int last_digit = abs(n) % 10;
 
 	if (last_digit > 5)
-{	printf("The last digit of %d is %d and is greater than 5\n", n, last_digit); }
-	else if (last_digit = 0)
+{	printf("The last digit of %d is %d and is greater than 5\n",
+	n, last_digit); }
+	else if (last_digit == 0)
 {	printf("The last digit of %d is %d and is 0\n", n, last_digit); }
-	else if (last_digit < 6) && (last_digit != 0)
-{	printf("The last digit of %d is %d and is less than 6 and not 0\n", n, last_digit); }
+	else if (last_digit < 6)
+	&&(last_digit != 0)
+{       printf("The last digit of %d is %d and is less than 6 and not 0\n",
+	n, last_digit); }
+	else
+{	printf("The last digit is %d\n", n); }
 	return (0);
 }
