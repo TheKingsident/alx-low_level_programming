162c5f246bf817715257589c096e39b969277d41
Kingsley Usa <kingsident@gmail.com>
2023-03-27 20:40:53 +0100

    Added new assignment directory and files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; alphabet[i] != '\0'; i++)
+{	putchar(alphabet[i]); }
+
+	putchar("\n");
+
+	return (0);
+}
