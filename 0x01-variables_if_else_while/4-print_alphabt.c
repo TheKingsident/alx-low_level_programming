c7fc31694a876f18c139bdaba6d467a87b729b7e
Kingsley Usa <kingsident@gmail.com>
2023-03-27 21:07:09 +0100

    Added new assignment directory and files

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,7 +6,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	char alphabet[] = "abcdfghijklmnoprstuvwxyz"
+{	char alphabet[] = "abcdfghijklmnoprstuvwxyz";
 	int i;
 
 	for (i = 0; alphabet[i] != '\0'; i++)
