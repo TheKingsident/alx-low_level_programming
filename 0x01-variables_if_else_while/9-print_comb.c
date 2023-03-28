0de5c8eba782e5de35bbedacfffa5f44d16a9fad
Kingsley Usa <kingsident@gmail.com>
2023-03-28 10:41:55 +0100

    Added new assignment directory and files

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	int i;
+	int j;
+	char commaspace[] = ", ";
+
+	for (i = 0; i < 100; i++)
+{	putchar(i + '\0');
+	for (j = 0; commaspace[j] != '\0'; j++)
+	{	putchar(commaspace[j]); } }
+
+	purchar('\n');
+	return (0);
+}
