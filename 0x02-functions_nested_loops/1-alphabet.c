44f16a83004bca2324549a671affeeab246cf815
Kingsley Usa <kingsident@gmail.com>
2023-03-29 19:45:44 +0100

    Added new assignment directory and files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
+#include <stdio.h>
 #include "main.h"
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{	print_alphabet();
+void print_alphabet(void)
+{	int i;
+	char something[] = "abcdefghijklmnopqrstuvwxyz";
 
-	return (0);
-}
+	for (i = 0; something[i] != '\0'; i++)
+{	putchar(something[i]); }
+
+	putchar('\n');
+}`
