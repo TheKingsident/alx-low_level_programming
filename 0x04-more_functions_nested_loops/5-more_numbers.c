1e7ee95c9ce5dc275e8fd06632af4c560123b670
Kingsley Usa <kingsident@gmail.com>
2023-05-25 19:15:12 +0100

    Updated asignment files

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * more_numbers - Prints numbers to console
+ *
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{	int i = 0;
+	int j;
+	char more_numbers[] = "01234567891011121314";
+
+	while (i < 11)
+
+	{	i++;
+		for (j = 0; more_numbers[j] != '\0'; j++)
+		{
+			_putchar(more_numbers[j]); }
+		_putchar('\n'); }
+
+	_putchar('\n');
+
+}
