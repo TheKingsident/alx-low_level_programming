3994e9414db5d3ffc15a2eeb34f311822a9b47d0
Kingsley Usa <kingsident@gmail.com>
2023-05-25 23:06:06 +0100

    Updated asignment files

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,27 +6,34 @@
  * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
 	int i;
-    for (i = 1; i <= 100; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            printf("FizzBuzz");
-        } else if (i % 3 == 0) {
-            printf("Fizz");
-        } else if (i % 5 == 0) {
-            printf("Buzz");
-        } else {
-            printf("%d", i);
-        }
 
-        if (i != 100) {
-            printf(" ");
-        } else {
-            printf("\n");
-        }
-    }
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz"); }
 
-    return 0;
+		else if (i % 3 == 0)
+		{
+			printf("Fizz"); }
+		else if (i % 5 == 0)
+		{
+			printf("Buzz"); }
+
+		else
+		{
+			printf("%d", i); }
+
+		if (i != 100)
+		{
+			printf(" "); }
+
+		else
+		{
+			printf("\n"); }
+	}
+	return (0);
 }
-
