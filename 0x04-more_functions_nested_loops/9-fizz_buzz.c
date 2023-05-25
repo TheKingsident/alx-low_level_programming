9a54dd99a0b2f48679d9859bb8699344467bdf0f
Kingsley Usa <kingsident@gmail.com>
2023-05-25 22:39:22 +0100

    Updated asignment files

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,11 +6,11 @@ int main()
 
 	for (i = 1; i < 101; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && (i % 5 != 0))
 		{
 			printf("Fizz ");
 		}
-		else if (i % 5 == 0)
+		else if ((i % 5 == 0) && (i % 3 != 0))
 		{
 			printf("Buzz ");
 		}
