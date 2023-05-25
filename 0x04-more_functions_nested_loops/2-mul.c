c424cd0a5bd47212924a0187e1631044fc2526d4
Kingsley Usa <kingsident@gmail.com>
2023-05-25 15:50:57 +0100

    Updated asignment files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - checks if a character is lowercase
+ *
+ * @a: the first integer
+ *
+ * @b: the second integer
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
