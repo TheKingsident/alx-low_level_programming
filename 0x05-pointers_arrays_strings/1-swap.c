c1fccb91f3633b82cf6a523558d9a3fb11eb174e
Kingsley Usa <kingsident@gmail.com>
2023-05-29 16:29:37 +0100

    Added new assigment files and directory

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *swap_int - swaps the assigned values in pointers a and b
+ *
+ *@a: pointer a
+ *
+ *@b: pointer b
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+
+}
