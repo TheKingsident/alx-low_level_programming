a79cd346b3f7952037524c792b8687c54de4bb25
Kingsley Usa <kingsident@gmail.com>
2023-05-29 22:36:50 +0100

    Added new assigment files and directory

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,16 +10,15 @@
 void print_array(int *a, int n)
 {
 	int i;
-	
+
 	if (n <= 0)
-        return;
+		return;
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		
+
 		if (i != n - 1)
-			
 			printf(", "); }
 	printf("\n");
 }
