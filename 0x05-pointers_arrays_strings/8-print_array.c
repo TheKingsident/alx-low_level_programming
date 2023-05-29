63da9b4b238cea136d7f2f20a2aae821f92036ed
Kingsley Usa <kingsident@gmail.com>
2023-05-29 22:34:07 +0100

    Added new assigment files and directory

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_array - Displays the elements in an array
  *
@@ -9,45 +10,16 @@
 void print_array(int *a, int n)
 {
 	int i;
-	int digit;
-	int divisor;
+	
+	if (n <= 0)
+        return;
 
-	for (i = 0; i < n; i ++)
+	for (i = 0; i < n; i++)
 	{
-		int numb = a[i];
-
-		if (numb == 0)
-		{
-			_putchar('0'); }
-
-		else if (numb < 0)
-		{
-			_putchar('-');
-			numb = -numb; }
-
+		printf("%d", a[i]);
 		
-		divisor = 1;
-
-		while (numb / divisor != 0)
-		{
-			divisor *= 10; }
-		
-		divisor /= 10;
-
-		while (divisor != 0)
-		{
-			digit = numb / divisor;
-			numb %= divisor;
-			divisor /= 10;
-			_putchar('0'+ digit); }
-
 		if (i != n - 1)
-		{
-			_putchar(',');
-	       		_putchar(' '); }
-
-	_putchar(' '); }
-
-	_putchar('\n');
-
+			
+			printf(", "); }
+	printf("\n");
 }
