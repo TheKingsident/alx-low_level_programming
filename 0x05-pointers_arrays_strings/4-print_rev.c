b2fa4c3b49104953454561ea7ecb4d319d0d4ee4
Kingsley Usa <kingsident@gmail.com>
2023-05-29 17:37:48 +0100

    Added new assigment files and directory

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - Displays some characters to console in reverse
+ *
+ * @s: Characters to display
+ */
+void print_rev(char *s)
+{
+	int i;
+	int j;
+	int length;
+
+	length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++; }
+
+	for (j = length; j >= 0; j--)
+	{
+		_putchar(s[j]); }
+
+	_putchar('\n');
+
+}
