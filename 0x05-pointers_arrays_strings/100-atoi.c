f8c3717e8fb67f37b3ab1af380e8300f32bc5b66
Kingsley Usa <kingsident@gmail.com>
2023-05-30 14:26:06 +0100

    Updated assignment files

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -22,23 +22,24 @@ int _atoi(char *s)
 		i++; }
 
 
-	if (s[i] && s[i] == '2' && s[i + 1] && s[i + 1] == '1' 
-			&& s[i + 2] && s[i + 2] == '4' && s[i + 3] && 
+	if (s[i] && s[i] == '2' && s[i + 1] && s[i + 1] == '1'
+			&& s[i + 2] && s[i + 2] == '4' && s[i + 3] &&
 			s[i + 3] == '7' && s[i + 4] && s[i + 4] == '4' &&
-		       	s[i + 5] && s[i + 5] == '8' && s[i + 6] && s[i + 6] 
-			== '3' && s[i + 7] && s[i + 7] == '6' && s[i + 8] && 
+			s[i + 5] && s[i + 5] == '8' && s[i + 6] && s[i + 6]
+			== '3' && s[i + 7] && s[i + 7] == '6' && s[i + 8] &&
 			s[i + 8] == '4' && s[i + 9] && s[i + 9] == '8')
 	{
-		return INT_MIN; }
+		return (INT_MIN); }
 
 
 
 	while (s[i] && s[i] >= '0' && s[i] <= '9')
 	{
-		if (result > (INT_MAX / 10) || (result == INT_MAX / 10 && (s[i] - '0') > (INT_MAX % 10)))
+		if (result > (INT_MAX / 10) || (result == INT_MAX / 10 &&
+					(s[i] - '0') > (INT_MAX % 10)))
 		{
 			fprintf(stderr, "Integer overflow detected.\n");
-			return 0; }
+			return (0); }
 
 		result = result * 10 + (s[i] - '0');
 		i++; }
