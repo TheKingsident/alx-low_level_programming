5f9bfb8c35343552d90b01d7047ee3a700adad46
Kingsley Usa <kingsident@gmail.com>
2023-06-01 19:12:28 +0100

    Added new assignment files

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,17 +15,17 @@ char *cap_string(char *str)
 	while (str[i] != '\0')
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-        {
-            if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
-                str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' ||
-                str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' ||
-                str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
-                str[i - 1] == '{' || str[i - 1] == '}')
-            {
-                str[i] = str[i] - ('a' - 'A');
-            }
-        }  
-
+		{
+			if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
+					str[i - 1] == '\n' || str[i - 1] == ','
+					|| str[i - 1] == ';' || str[i - 1] ==
+					'.' || str[i - 1] == '!' || str[i - 1]
+					== '?' || str[i - 1] == '"' ||
+					str[i - 1] == '(' || str[i - 1] == ')' ||
+					str[i - 1] == '{' || str[i - 1] == '}')
+			{
+				str[i] = str[i] - ('a' - 'A'); }
+		}
 		i++; }
 
 	return (str);
