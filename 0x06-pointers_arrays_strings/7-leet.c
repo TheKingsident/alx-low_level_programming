d3c36fa42246085bdd03a8856a87cd2331d037f4
Kingsley Usa <kingsident@gmail.com>
2023-06-01 20:57:04 +0100

    Added new assignment files

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * leet - A function that encodes a string into 1337
+ *
+ * @str: stings to encode
+ *
+ * Return: The results
+ */
+char *leet(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+		{
+			str[i] = '4'; }
+		
+		else if (str[i] == 'e' || str[i] == 'E')
+		{
+			str[i] = '3'; }
+
+		else if (str[i] == 'o' || str[i] == 'O')
+		{
+			str[i] = '0'; }
+		
+		else if (str[i] == 't' || str[i] == 'T')
+		{
+			str[i] = '7'; }
+
+		else if (str[i] == 'l' || str[i] == 'L')
+		{
+			str[i] = '1'; }
+
+		i++;
+
+	}
+
+	return (str);
+
+}
