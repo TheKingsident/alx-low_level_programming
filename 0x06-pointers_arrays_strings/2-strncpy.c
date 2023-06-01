048c78bde019bd76f1f8ff7c0987353af3d1cb2e
Kingsley Usa <kingsident@gmail.com>
2023-06-01 15:23:15 +0100

    Added new assignment files

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -19,9 +19,12 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for(i = 0; i < n; i++)
 	{
-		dest[i] = src[i]; }
+		if(src[i] != '\0')
+			
+			dest[i] = src[i];
 
-	dest[n] = '\0';
+		else
+			dest[i] = '\0';	}
 
 	return (dest);
 }
