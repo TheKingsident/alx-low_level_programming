a31ade779614b1ff36485270b7b49c70e6189ce3
Kingsley Usa <kingsident@gmail.com>
2023-06-01 19:03:08 +0100

    Added new assignment files

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,18 +14,15 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-		if ((str[i] == ',' || str[i] == ' ' || str[i] == '\n' || str[i]
-				== ';' || str[i] == '.' || str[i] == '!' ||
-				str[i] == '?' || str[i] == '"' || str[i] ==
-				'(' || str[i] == ')' || str[i] == '{' ||
-				str[i] == '}' || str[i] == '\t') &&
-				(str[i + 1] >= 'a' && str[i + 1] <= 'z'))
-		{
-			str[i + 1] = str[i + 1] - ('a' - 'A'); }
-
-		else
-			{
-				str[i] = str[i]; }
+		if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            if (i == 0 || !((str[i - 1] >= 'a' && str[i - 1] <= 'z') ||
+                           (str[i - 1] >= 'A' && str[i - 1] <= 'Z') ||
+                           (str[i - 1] >= '0' && str[i - 1] <= '9')))
+            {
+                str[i] = str[i] - ('a' - 'A');
+            }
+        }
 
 		i++; }
 
