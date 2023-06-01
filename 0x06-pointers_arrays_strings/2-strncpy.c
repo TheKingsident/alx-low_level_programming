8b7b1aac0e407f9d6cb06f205482c58e414bc24e
Kingsley Usa <kingsident@gmail.com>
2023-06-01 15:07:47 +0100

    Added new assignment files

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strncpy - Copies the string in src into dest
+ *
+ * depending on n number of bytes
+ *
+ * @src: the string to copy
+ *
+ * @dest: destination to copy into
+ *
+ * @n: number of bytes
+ *
+ * Return: the dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for(i = 0; i < n; i++)
+	{
+		dest[i] = src[i]; }
+
+	return (dest);
+}
