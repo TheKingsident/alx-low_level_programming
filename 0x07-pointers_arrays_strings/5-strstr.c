92697415f8e7a2e3e1066e88450f36e3a8fea3d2
Kingsley Usa <kingsident@gmail.com>
2023-06-05 18:36:02 +0100

    Added assignment files

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,28 +13,24 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 	int j;
-	int needleLength;
-
-	needleLength = 0;
-
-	while (needle[needleLength] != '\0')
-
-		needleLength++;
-
+	
 	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		if (haystack[i] == needle[0])
-		{
-			for (j = 1; j <= needleLength; j++)
-			{
-				if (haystack[i + j] != needle[j])
+    {
+       
+        if (haystack[i] == needle[0])
+        {
+            
+            for (j = 0; needle[j] != '\0'; j++)
+            {
+                if (haystack[i + j] != needle[j])
+                    break;
+            }
 
-					break; }
-
-			if (j > needleLength)
-
-				return (&haystack[i]); }
-	}
+            
+            if (needle[j] == '\0')
+                return &haystack[i];
+        }
+    }
 
 	return (NULL);
 
