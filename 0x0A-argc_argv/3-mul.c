de1aaafaa1ec953615d285837226b4264a48b2c7
Kingsley Usa <kingsident@gmail.com>
2023-06-09 13:11:26 +0100

    Added assignment fiels

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Main function
+ *
+ * @argc: Argument counter
+ *
+ * @argv: Argument vector
+ *
+ * Return: Always 0 for successful completion
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul;
+
+	mul = 1;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+
+		return (1); }
+
+	for (i = 1; i < argc; i++)
+	{
+		mul *= atoi(argv[i]); }
+
+	printf("%d\n", mul);
+
+	return (0);
+}
