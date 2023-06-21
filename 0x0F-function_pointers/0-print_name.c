62e41ace5711a6aed5533f2ae8e8d37320cff2d9
Kingsley Usa <kingsident@gmail.com>
2023-06-21 14:22:44 +0100

    Added new assignment files and directory

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - Function to print a name
+ *
+ * @name: The name to be printed
+ *
+ * @f: Pointer to function that does the heavy lifting
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
