e5e963fe02f7fa5bbdd40f4c78f83755e6896f3b
Kingsley Usa <kingsident@gmail.com>
2023-06-23 16:41:03 +0100

    Added new assignment files

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -17,11 +17,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	i = 0;
 
-	if (array != NULL)
+	if (array != NULL && action != NULL)
 	{
 		while (i < size)
 		{
 			action(array[i]);
 			i++; }
 	}
+
 }
