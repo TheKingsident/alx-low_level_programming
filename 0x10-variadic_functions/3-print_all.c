7878bef90f6c49188118b669422ceb2a5fe60ffd
Kingsley Usa <kingsident@gmail.com>
2023-06-23 12:52:33 +0100

    Added new assignment files

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,43 @@
+#include "variadic_functions.h"
+/**
+ *  print_all - Prints all sorts of data types passed as arguments
+ *  @format: is a list of types of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *str;
+
+	i = 0;
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+			if (str == NULL)
+				str = "(nil)";
+			printf("%s", str);
+			break; }
+
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i]
+					== 's') && format[i + 1] != '\0')
+			printf(", ");
+		i++; }
+
+	printf("\n");
+	va_end(args);
+}
