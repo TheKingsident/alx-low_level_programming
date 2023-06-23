8e7451283c823275e5f7c7c3bc2bbcea4ac54a8d
Kingsley Usa <kingsident@gmail.com>
2023-06-23 12:18:56 +0100

    Added new assignment files

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -40,9 +40,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			else
 			{
 				printf("%s", name); }
-			if (i != (n - 1))
-			{
-				printf(" "); }
 		}
 	}
 	printf("\n");
