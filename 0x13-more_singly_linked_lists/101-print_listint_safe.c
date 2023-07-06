bb588ff4aa95b20995e6278b14720c0e97c166f8
Kingsley Usa <kingsident@gmail.com>
2023-07-06 20:38:03 +0100

    Added new assignment directory and files

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,29 +6,32 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *currentNode;
 	const listint_t *checkingNode;
 	size_t nodeCount;
+	int loop;
 
 	currentNode = head;
 	checkingNode = head;
 	nodeCount = 0;
+	loop = 0;
 
 	while (currentNode != NULL)
 	{
+		if (loop && checkingNode == currentNode)
+		{
+			 printf("-> [%p] %d\n", (void *)currentNode, currentNode->n);
+			 return (nodeCount); }
+
 		printf("[%p] %d\n", (void *)currentNode, currentNode->n);
 		nodeCount++;
 
-		if (checkingNode != NULL && currentNode != NULL)
+		if (checkingNode != NULL && checkingNode->next != NULL)
 		{
 			checkingNode = checkingNode->next->next;
-			currentNode = currentNode->next; }
+			currentNode = currentNode->next;
+	       		loop = 1; }
 		else
 		{
 			currentNode = currentNode->next;
-		}
-
-		if (checkingNode == currentNode)
-		{
-			printf("-> [%p] %d\n", (void *)currentNode, currentNode->n);
-			exit(98);
+			loop = 0;
 		}
 	}
 
