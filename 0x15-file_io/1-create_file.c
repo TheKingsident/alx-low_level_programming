da3b913621e26139582651e5cbff15d89316e5eb
Kingsley Usa <kingsident@gmail.com>
2023-07-11 02:24:34 +0100

    Added new assignment files and directory

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1); }
 
-	fileDescriptor == open(filename, O_WRONLY | O_CREAT | O_TRUNC,
+	fileDescriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC,
 			filePermissions);
 
 	if (fileDescriptor == -1)
