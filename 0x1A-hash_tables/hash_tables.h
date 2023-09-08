76a1cd8172f0991edb853d6ef0e6e5b5678ab1b7
Kingsley Usa <77732519+TheKingsident@users.noreply.github.com>
2023-09-08 23:54:10 +0100

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -41,5 +41,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+shash_node_t *shash_node_create(const char *key, const char *value);
 
 #endif
