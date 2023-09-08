5c9f8a4044d333105b058a9ef019bc3df35b3e08
Kingsley Usa <kingsident@gmail.com>
2023-09-08 21:38:32 +0100

    Added new files

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	if (ht == NULL)
+		return;
+
+	for (unsigned long int i = 0; i < ht->size; i++)
+	{
+		hash_node_t *currentNode = ht->array[i];
+
+		while (currentNode != NULL)
+		{
+			hash_node_t *temp = currentNode;
+
+			currentNode = currentNode->next;
+
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
